b7cd221845555acb7ffca6be07a9acf1681d24ab
EliasEF <eliasef0305@gmail.com>
2022-10-26 20:16:45 -0300

    :gem: feat: req 2

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+SELECT
+	COUNT(DISTINCT mu.musica_id) AS 'cancoes',
+    COUNT(DISTINCT ar.artista_id) AS 'artistas',
+    COUNT(DISTINCT al.album_id) AS 'albuns'
+FROM
+	SpotifyClone.musica AS mu,
+    SpotifyClone.artista AS ar,
+    SpotifyClone.album AS al;
