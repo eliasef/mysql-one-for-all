4d1e145fc5d695df7c86918234cac75ac024ee97
EliasEF <eliasef0305@gmail.com>
2022-10-30 17:07:10 -0300

    :gem: feat: req 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,1 +1,9 @@
-
+SELECT
+    ROUND(MIN(pl.preco), 2) as faturamento_minimo,
+    ROUND(MAX(pl.preco), 2) as faturamento_maximo,
+    ROUND(AVG(pl.preco), 2) as faturamento_medio,
+    ROUND(SUM(pl.preco), 2) AS faturamento_total
+FROM
+    `SpotifyClone`.usuario AS us
+INNER JOIN
+    `SpotifyClone`.plano AS pl ON pl.plano_id = us.plano_id;
