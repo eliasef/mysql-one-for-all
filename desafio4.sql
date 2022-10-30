5b8a55ec6b4cd88cc7a29376bb708756c7ba3307
EliasEF <eliasef0305@gmail.com>
2022-10-29 22:32:27 -0300

    :gem: feat: req 4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+SELECT
+    us.nome AS usuario,
+    IF (MAX(YEAR(hi.data_reproducao)) >= '2021', "Usuário ativo", "Usuário inativo" ) AS status_usuario
+FROM
+    SpotifyClone.usuario AS us
+INNER JOIN
+    SpotifyClone.historico_reproducoes AS hi ON us.usuario_id = hi.usuario_id
+GROUP BY
+    us.nome
+ORDER BY
+    us.nome;
